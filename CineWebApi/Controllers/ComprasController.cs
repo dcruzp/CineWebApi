1365b0d4d646285b71952c6a6d8d78d2eba2a369
Frank Perez <frankadrian98@gmail.com>
2021-06-15 20:43:52 +0000

    HAciendo el compras controller

---
+++ CineWebApi/Controllers/ComprasController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CineWebApi.DBModels;
+using System.Threading.Tasks;
+
+namespace CineWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+
+        // GET: api/<ComprasController>
+        [HttpGet]
+        public async Task<ActionResult<Compra[]>> Get()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+    }      
+    
+}
